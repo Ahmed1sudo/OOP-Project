1ac79a4429df87d4554b27d15417d247bd81a94e
aa <aa>
2025-07-21 16:02:31 +0400

    Task1

--- OOP Task/Task1.java
+++ OOP Task/Task1.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class Task1 {
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        p1.name = "Alice";
+        p1.age = 25;
+
+        // عرض البيانات
+        p1.display();
+    }
 }
+
+// تعريف الكلاس Person
+class Person {
+    String name;
+    int age;
+
+    // دالة لعرض البيانات
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
+
